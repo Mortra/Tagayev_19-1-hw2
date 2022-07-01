54d1bb6cea5722945fba3bd40b00e3f97d190c4c
Mortra <107037848+Mortra@users.noreply.github.com>
2022-07-02 00:53:00 +0600

    Add files via upload

---
+++ hw - 3 .py
@@ -0,0 +1,15 @@
+class Car:
+    def __init__(self, title, model):
+        self.title = title
+        self.model = model
+
+class Cars(Car):
+    def __init__(self, title, model, color):
+        super().__init__(title, model)
+        self.color = color
+    def start_engine(self):
+        print(f"Engine on {self.title} {self.model} {self.color} started!")
+    def stop_engine(self):
+        print(f"Engine on {self.title} {self.model} {self.color} stoped!\n")
+    def get_car_info(self):
+        print (f'\n{self.title} {self.model} {self.color}')
